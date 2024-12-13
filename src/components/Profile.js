248b8bf2f234a946e9855a289ac117626b8b7ffe
pierre gourinat <pierregourinat@gmail.com>
2024-12-13 11:28:34 +0100

    init

---
+++ src/components/Profile.js
@@ -0,0 +1,15 @@
+import "../styles/Profile.css";
+
+function Profile() {
+  return (
+    <div className="profileWrapper">
+      <div>Logo</div>
+      <div>Pierre Gourinat</div>
+      <div>Intégrateur web junior</div>
+      <div>pierregourinat@gmail.com</div>
+      <div>06 86 01 25 88</div>
+    </div>
+  );
+}
+
+export default Profile;
