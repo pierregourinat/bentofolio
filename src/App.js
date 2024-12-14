fae10889386a617b4290d94ec886500a4f234660
pierre gourinat <pierregourinat@gmail.com>
2024-12-14 17:01:29 +0100

    layout placeholders

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import "./App.css";
+import Formation from "./components/Formation";
 import Profile from "./components/Profile";
 import SocialsBtn from "./components/SocialsBtn";
 
 function App() {
   return (
-    <div className="upperContainer">
-      <Profile />
-      <div className="skillsWrapper">
+    <div className="bentoContainer">
+      <div className="upperContainer">
         <Profile />
-        <div className="skillsLowerSection">
+        <div className="skillsWrapper">
           <Profile />
-          <Profile />
+          <div className="skillsLowerSection">
+            <Profile />
+            <Profile />
+          </div>
+        </div>
+        <div className="btnWrapper">
+          <SocialsBtn />
+          <SocialsBtn />
+          <SocialsBtn />
         </div>
       </div>
-      <div className="btnWrapper">
-        <SocialsBtn />
-        <SocialsBtn />
-        <SocialsBtn />
+      <div className="lowerContainer">
+        <Formation />
+        <Formation />
+        <Formation />
       </div>
     </div>
   );
---
+++ src/components/Formation.js
@@ -0,0 +1,22 @@
+import "../styles/Formation.css";
+
+function Formation() {
+  return (
+    <div className="formationWrapper">
+      <div>
+        <h2>PARCOURS INTÉGRATEUR WEB</h2>
+        <p>
+          En cours : certification professionnelle “Développeur Informatique“
+        </p>
+      </div>
+      <div>
+        <h2>PARCOURS INTÉGRATEUR WEB</h2>
+        <p>
+          En cours : certification professionnelle “Développeur Informatique“
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Formation;
