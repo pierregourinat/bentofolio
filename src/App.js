0e96829ce0e6b42389f4ffb9edee13fcf92d32fb
pierre gourinat <pierregourinat@gmail.com>
2024-12-18 19:11:50 +0100

    feat : merge presentation et socialsBtn

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import BackgroundImage from "./components/BackgroundImage";
 import GitHubProjects from "./components/GitHubProjects";
 import Profile from "./components/Profile";
-import SocialsBtn from "./components/SocialsBtn";
 import backgroundImage from "./assets/images/mykyta-martynenko-_1UF_3TlKcQ-unsplash.jpg";
 import Presentation from "./components/Presentation";
 import Skills from "./components/Skills";
@@ -16,7 +15,7 @@ function App() {
         <Profile />
         <Presentation />
         <Skills />
-        <SocialsBtn />
+
         <Career />
         <GitHubProjects />
       </div>
--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -1,21 +1,25 @@
 import "../styles/Presentation.css";
+import SocialsBtn from "./SocialsBtn";
 
 function Presentation() {
   return (
     <div className="presentationWrapper backgroundGradient">
-      <h2>PRESENTATION</h2>
-      <p>
-        Passioné par le web design, je souhaite mettre mon expérience et mes
-        nouvelles compétences techniques au service de l'intégration web et la
-        création d'interfaces optimisées.
-      </p>
-      <p>Mes mots-clefs :</p>
+      <div>
+        <h2>PRESENTATION</h2>
+        <p>
+          Passioné par le web design, je souhaite mettre mon expérience et mes
+          nouvelles compétences techniques au service de l'intégration web et la
+          création d'interfaces optimisées.
+        </p>
+      </div>
+      <SocialsBtn />
+      {/* <p>Mes mots-clefs :</p>
       <ul>
         <li>Frontend dev</li>
         <li>Responsive design</li>
         <li>Web design</li>
         <li>UX/UI</li>
-      </ul>
+      </ul> */}
     </div>
   );
 }
