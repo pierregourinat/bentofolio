c38ee0ee74ec085b244fac002117ce5767e7c0a6
pierre gourinat <pierregourinat@gmail.com>
2024-12-28 16:09:19 +0100

    feat : tech cards githubprojects

--- src/components/GitHubProjects.jsx
+++ src/components/GitHubProjects.jsx
@@ -1,6 +1,17 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import PropTypes from 'prop-types';
 import projectsData from "../data/projects.json";
 import "../styles/GitHubProjects.css";
+
+const TechBadge = ({ tech }) => (
+  <span className="techBadge">
+    {tech}
+  </span>
+);
+
+TechBadge.propTypes = {
+  tech: PropTypes.string.isRequired
+};
 
 const ProjectCard = ({ project }) => {
   const [isHovered, setIsHovered] = useState(false);
@@ -18,8 +29,22 @@ const ProjectCard = ({ project }) => {
     >
       <h3 className="projectCardTitle">{project.name}</h3>
       <div className="projectCardDescription">{project.description}</div>
+      <div className="techBadgesContainer">
+        {project.technologies?.map((tech, index) => (
+          <TechBadge key={index} tech={tech} />
+        ))}
+      </div>
     </div>
   );
+};
+
+ProjectCard.propTypes = {
+  project: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    technologies: PropTypes.arrayOf(PropTypes.string),
+    githubLink: PropTypes.string.isRequired
+  }).isRequired
 };
 
 const GitHubProjects = () => {
